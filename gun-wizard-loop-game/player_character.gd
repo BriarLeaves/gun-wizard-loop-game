ecf4cf129aa82078b320d5cb3a1f800a2d6cc265
Marcelo Faria <mfariagamedev@gmail.com>
2025-07-31 00:21:35 -0300

    Added bullets, shooting, and made small changes to player movement
    Added the bullet scene, added the functionality to shoot the bullet scene from the player, and made some small syntax changes to the player scene

--- gun-wizard-loop-game/player_character.gd
+++ gun-wizard-loop-game/player_character.gd
@@ -2,7 +2,14 @@ extends CharacterBody2D
 
 var x_axis: float = 0
 var y_axis: float = 0
-var move_vector: Vector2 = Vector2(0, 0)
+var move_vector: Vector2 = Vector2.ZERO
+var mouse_position: Vector2 = Vector2.ZERO
+var player_speed: float = 1
+
+var bullet_direction: Vector2 = Vector2.ZERO
+var bullet_speed: float = 3
+
+@onready var bullet = preload("res://bullet.tscn")
 
 func _ready():
 	pass
@@ -11,13 +18,21 @@ func _process(delta):
 	x_axis = 0
 	y_axis = 0
 	if Input.is_action_pressed("move_right"):
-		x_axis += 1
+		x_axis += player_speed
 	if Input.is_action_pressed("move_left"):
-		x_axis -= 1
+		x_axis -= player_speed
 	if Input.is_action_pressed("move_up"):
-		y_axis -= 1
+		y_axis -= player_speed
 	if Input.is_action_pressed("move_down"):
-		y_axis += 1
+		y_axis += player_speed
+		
+	if Input.is_action_just_pressed("shoot"):
+		mouse_position = get_viewport().get_mouse_position()
+		bullet_direction = (mouse_position - position).normalized()
+		var bullet_instance = bullet.instantiate()
+		bullet_instance.velocity = bullet_direction * bullet_speed
+		bullet_instance.position = position
+		$"../BulletManager".add_child(bullet_instance)
 	pass
 
 func _physics_process(delta):
