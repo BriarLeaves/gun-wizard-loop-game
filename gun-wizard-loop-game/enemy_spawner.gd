3520fd572e75d142c49503c1d2b95b531d13a164
Marcelo Faria <mfariagamedev@gmail.com>
2025-08-03 10:28:10 -0300

    Added a timer that spawns enemies depending on time elapsed

--- gun-wizard-loop-game/enemy_spawner.gd
+++ gun-wizard-loop-game/enemy_spawner.gd
@@ -5,22 +5,28 @@ var current_enemies: Array[PackedScene]
 
 @onready var screen_size: Vector2 = get_viewport_rect().size
 
+@onready var player = get_node("../ScoreTimer")
+var time_elapsed: float = 0.0
+
+var time_spawn_digging: float = 15.0
+var time_spawn_exploding: float = 30.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_enemies.append(enemies[0])
-	#current_enemies.append(enemies[1])
-	current_enemies.append(enemies[2])
-	current_enemies.append(enemies[3])
 
 # Called every frame. 'delta' is the elapsed time since the prwevious frame.
 func _process(delta: float) -> void:
-	pass
+	time_elapsed += delta
+	if time_elapsed >= time_spawn_digging:
+		current_enemies.append(enemies[1])
+	if time_elapsed >= time_spawn_exploding:
+		current_enemies.append(enemies[2])
 
 func _on_timer_timeout() -> void:
 	var enemyType = randi_range(0, current_enemies.size() - 1)
 	var enemy = current_enemies[enemyType].instantiate()
 	
-	#if enemy == current_enemies[0] || enemy == current_enemies[1]:
 	if enemyType == 0:
 		var enemy_spawn_location = get_node("SpawnPath/SpawnLocation")
 		enemy_spawn_location.progress_ratio = randf()
@@ -28,7 +34,7 @@ func _on_timer_timeout() -> void:
 		enemy.initialize(enemy_spawn_location.position)
 	else:
 		var enemy_spawn_location = Vector2(randf_range(0, screen_size.x), randf_range(0, screen_size.y))
-		while enemy_spawn_location.distance_to(get_node("Player").position) < 50:
+		while enemy_spawn_location.distance_to(get_node("Player").position) < 75:
 			enemy_spawn_location = Vector2(randf_range(0, screen_size.x), randf_range(0, screen_size.y))
 		enemy.initialize(enemy_spawn_location)
 	add_child(enemy)
