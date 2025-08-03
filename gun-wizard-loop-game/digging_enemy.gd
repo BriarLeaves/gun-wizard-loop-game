c43c4246d859ad4801c9ed0479ba58b540b505fe
Marcelo Faria <mfariagamedev@gmail.com>
2025-08-03 00:56:14 -0300

    Added safe zone around player to spawn enemies and stopped spawning after player dies

--- gun-wizard-loop-game/digging_enemy.gd
+++ gun-wizard-loop-game/digging_enemy.gd
@@ -1,8 +1,11 @@
 extends Enemy
+class_name DiggingEnemy
 
 var speed: float = 300
 var direction_vector: Vector2 = Vector2.ZERO
 @onready var player = get_node("../Player")
+var collision
+var area2d
 
 func _ready():
 	pass
@@ -13,8 +16,6 @@ func _physics_process(delta):
 
 func initialize(start_position):
 	position = start_position
-	get_node("CollisionShape2D").set_deferred("disabled", true)
-	get_node()
 
 
 func _on_timer_timeout() -> void:
--- gun-wizard-loop-game/enemy_spawner.gd
+++ gun-wizard-loop-game/enemy_spawner.gd
@@ -19,6 +19,10 @@ func _on_timer_timeout() -> void:
 	var enemyType = randi_range(0, current_enemies.size() - 1)
 	var enemy = current_enemies[enemyType].instantiate()
 	
+	if get_node("Player"):
+		$Timer.stop()
+		pass
+	
 	#if enemy == current_enemies[0] || enemy == current_enemies[1]:
 	if enemyType == 0:
 		var enemy_spawn_location = get_node("SpawnPath/SpawnLocation")
@@ -27,5 +31,7 @@ func _on_timer_timeout() -> void:
 		enemy.initialize(enemy_spawn_location.position)
 	else:
 		var enemy_spawn_location = Vector2(randf_range(0, screen_size.x), randf_range(0, screen_size.y))
+		while enemy_spawn_location.distance_to(get_node("Player").position) < 20:
+			enemy_spawn_location = Vector2(randf_range(0, screen_size.x), randf_range(0, screen_size.y))
 		enemy.initialize(enemy_spawn_location)
 	add_child(enemy)
